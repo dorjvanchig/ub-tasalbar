3ef961c5b4526ae40a8559933d1050ec11e0560e
dorjvanchig <bdorjvanchig@gmail.com>
2023-04-09 22:48:14 +0800

    zs

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,7 @@
 import '@/styles/globals.css'
 import '../styles/tailwind.css'
-import Tses from '@/src/tses/Tses'
 export default function App({ Component, pageProps }) {
   return <main>
-      {/* <Tses /> */}
       <Component {...pageProps} />
     </main>
 }
