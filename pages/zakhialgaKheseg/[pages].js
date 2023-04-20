3e81814721079b1234004f18654dd06182937768
dorjvanchig <bdorjvanchig@gmail.com>
2023-04-20 18:14:11 +0800

    zasvar

--- pages/zakhialgaKheseg/[pages].js
+++ pages/zakhialgaKheseg/[pages].js
@@ -1,10 +1,11 @@
 import React from 'react'
 import UndsenZagvar from '@/src/admin/undsenZagvar/UndsenZagvar'
 import { useRouter } from 'next/router'
+import SuudalSongokh from '@/src/zakhialgaUdirdlaga/SuudalSongokh'
 
 export default function Pages() {
   const router = useRouter()
   const {pages} = router.query
 
-  return (<div>222222</div>)
+  return (<SuudalSongokh {...pages}/>)
 }
---
+++ src/zakhialgaUdirdlaga/SuudalSongokh.js
@@ -0,0 +1,9 @@
+import React, {} from 'react'
+
+export default function SuudalSongokh(props) 
+{
+    return (<div className='bg-slate-300 w-full'>
+        
+        
+    </div>)    
+}
